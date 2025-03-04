0a3d88e6e65c50692bc950e7e38188c3ac18713d
김지혁 <kjeok@jeok-ui-MackBookPro.local>
2025-03-04 16:47:32 +0900

    init

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,12 @@
-import Home from "./pages/Home";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <Home />;
+  return (
+    <h1 className="text-red-500 font-bold underline">
+      Hello World!
+    </h1>
+  );
 }
 
 export default App;
--- my-app/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-      <footer className="w-full bg-gray-100 text-center py-4 mt-8">
-        © 2024 NAVER Clone
-      </footer>
-    );
-  };
-  
-  export default Footer;
